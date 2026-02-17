f161a3458713643d9eb2116784103e3f4048d4cd
TomasPlayBR <tomasmvb099@gmail.com>
2026-02-17 23:21:32 +0000

    Update script.js

--- public/script.js
+++ public/script.js
@@ -71,7 +71,7 @@ async function login() {
         else if (["chefe1"].includes(username)) role = "Chefe";
         else if (["agentecoordenador1"].includes(username)) role = "Agente Coordenador";
         else if (["agenteprincipal1"].includes(username)) role = "Agente Principal";
-        else if (["leandro", "rayzer", "mafu", "Viveiros", "Gui", "Afonso", "Pepsi", "Raul", "Silva", "Lopes", "ZéTó"].includes(username)) role = "Agente";
+        else if (["leandro", "rayzer", "mafu", "Viveiros", "Gui", "Afonso", "Pepsi", "Raul", "Silva", "Lopes", "ZéTó", "Silvazin"].includes(username)) role = "Agente";
         else if (["agenteprovisorio1"].includes(username)) role = "Agente Provisório";
 
         const userData = {
